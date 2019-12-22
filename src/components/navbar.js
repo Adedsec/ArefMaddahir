3a9475eb2d6b3a8c88cdfc9a78238bb0314c5083
dedsec <arefmaddah@gmail.com>
2019-12-22 23:17:39 +0330

    first commit(basic configurations done)

---
+++ src/components/navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+class NavBar extends React.Component {
+  render() {
+    return (
+      <div className="header">
+        <div className="logo"></div>
+      </div>
+    );
+  }
+}
+export default NavBar;
